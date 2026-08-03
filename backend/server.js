a9f2c63d877272166c7769ef6af807f5b5a4a274
Tobe Marizu <tobemarizu@gmail.com>
2026-08-03 17:41:54 +0100

    cors

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,11 @@ import userRoutes from "./routes/user.routes.js";
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+      origin:[ 
+        'http://localhost:5173',
+        'https://aebookify.vercel.app'
+    ], // your frontend URL
+
     credentials: true,
 }));
 app.use(express.json());
